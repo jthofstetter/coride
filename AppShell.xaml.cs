001429c7efbec6df614f42273ab18d9582aef124
Jannikasus <j.hofstetter@htlkrems.at>
2025-11-10 11:49:57 +0100

    Initial commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CoRide;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitialzeComponent();
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using CoRide;
+using Foundation;
+
+namespace CoRide;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
